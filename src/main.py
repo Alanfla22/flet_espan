ee162b4874c30de8b384b00feb1471e876585b19
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2025-03-21 00:50:51 -0300

    upload main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
-import time
 import flet as ft
-import spacy
 import random
 import os
 
-nlp = spacy.load('es_core_news_sm')
+
 app_data_path = os.getenv("FLET_APP_STORAGE_DATA")
 my_file_path = os.path.join(app_data_path, "data.txt")
 
 # função de atualização da lista
 def atualizar_lista():
   
-    arquivo = open(my_file_path, 'r')
+    arquivo = open(my_file_path, 'r', encoding='latin-1')
     lista_frase = arquivo.readlines()
     frase = lista_frase[0]
     lista_frase.remove(frase)
@@ -22,12 +20,15 @@ def atualizar_lista():
 
 # função tokenização
 def tokenizacao(frase):
-    doc = nlp(frase)
-    tokens = [token.text.casefold() for token in doc]
-    random.shuffle(tokens)
+
     for i in (['.', '\n', '?', '¿', ',', '¡', '!']):
-        if i in tokens:
-            tokens.remove(i)
+        if i in frase:
+            frase = frase.replace(i, '')
+
+    doc = frase.split(' ')
+    tokens = [token.casefold() for token in doc]
+    random.shuffle(tokens)        
+
     vocabulos = ''
     for token in tokens:
         vocabulos += token + ' '
@@ -41,7 +42,7 @@ def main(page: ft.Page):
     page.horizontal_alignment=ft.CrossAxisAlignment
     page.update()
 
-    frase = open(my_file_path, 'r').readlines()[0]
+    frase = open(my_file_path, 'r',encoding='latin-1').readlines()[0]
     vocabulos = tokenizacao(frase)
 
     a = ft.Container(
@@ -53,7 +54,7 @@ def main(page: ft.Page):
                     )  
 
     def revel(e):
-        frase = open(my_file_path, 'r').readlines()[0]
+        frase = open(my_file_path, 'r', encoding='latin-1').readlines()[0]
         c.content = ft.Text(value=frase, color="yellow", weight=ft.FontWeight.W_500)
         c.update()
 
@@ -68,7 +69,7 @@ def main(page: ft.Page):
                                                                                                             
     def atualizar(e):
         atualizar_lista()
-        frase = open(my_file_path, 'r').readlines()[0]
+        frase = open(my_file_path, 'r', encoding='latin-1').readlines()[0]
         vocabulos = tokenizacao(frase)
         a.content = ft.Text(value=vocabulos, color="grey", weight=ft.FontWeight.W_500)
         c.content=ft.Text(value='VER', color="yellow", weight=ft.FontWeight.W_500)
